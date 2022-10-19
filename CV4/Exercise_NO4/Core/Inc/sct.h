7726fcdda366a1d385062a0d861ffcfd51cd1a5b
Vladimír Lukáč <vlado1354@gmail.com>
2022-10-19 12:22:31 +0200

    Exercise_NO4

---
+++ CV4/Exercise_NO4/Core/Inc/sct.h
@@ -0,0 +1,16 @@
+/*
+ * sct.h
+ *
+ *  Created on: 12. 10. 2022
+ *      Author: xlukac14
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
